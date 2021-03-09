a5aa583438abd6b48426ea790ea2034754351a30
rasskazov-pv <rasskazov.paul@yandex.ru>
2021-03-09 03:59:42 +0300

    Определение конца игры

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ class TicTacToe
   def initialize
     @board = [0] * 9
     @current_player = 1
-    @equal_cells_in_final_states = [
+    @cells_sets_in_final_states = [
       # горизонтали
       [0, 1, 2],
       [3, 4, 5],
@@ -23,9 +23,25 @@ class TicTacToe
   def make_step(row, column, value)
     @board[(row * 3) + column] = value
   end
+
+  # метод определяет завершена ли игра
+  def is_final
+    # прохождение по массиву сетов с индексами ячеек
+    cell_check_result = @cells_sets_in_final_states.map do |cells_set|
+      #извлечение массивов со значениями контрольных ячеек с доски
+      cells_values = [@board[cells_set[0]], @board[cells_set[1]], @board[cells_set[2]]]
+
+      # возвращение массива с true\false , где true указывает на равенство всех трёх контрольных ячеек, не равных 0
+      # а false на неравенство этих ячеек
+      cells_values.uniq.length == 1 && cells_values.first != 0
+    end
+
+    cell_check_result.uniq.length == 2 || @board.index(0).nil?
+  end
 end
 
 game = TicTacToe.new
 p game.board
-game.make_step(1,2,1)
-p game.board
+
+p game.is_final
+game
