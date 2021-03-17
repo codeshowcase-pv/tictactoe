929375e875105fe6f6b296e6cc223d74dbd79f82
rasskazov-pv <rasskazov.paul@yandex.ru>
2021-03-17 07:17:27 +0300

    Фикс ошибки с неменяющимися ходами

--- queue_players.rb
+++ queue_players.rb
@@ -9,6 +9,6 @@ class QueuePlayers
   end
 
   def set_next_player_active
-    @active_player_index = @active_player_index + 1 > @players.count ? 0 : 1
+    @active_player_index = @active_player_index + 1 > @players.count - 1 ? 0 : 1
   end
 end
